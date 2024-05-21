882df8f2dddcd78abd9e0cbd27678b061d9ba150
RuslanRinatovich <safiullin@yandexlyceum.ru>
2024-05-21 17:58:36 +0300

    Осталось оформить диплом

---
+++ PharmacyApp/Pages/EduPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EduInstitutesApp.Models;
+using EduInstitutesApp.Windows;
+using System.Data.Entity;
+namespace EduInstitutesApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EduPage.xaml
+    /// </summary>
+    public partial class EduPage : Page
+    {
+        int _itemcount = 0;
+        public EduPage()
+        {
+            InitializeComponent();
+            LoadComboBoxItems();
+            LoadDataGrid();
+        }
+
+        void LoadDataGrid()
+        {
+            List<Organization> goods = OrganizationDBEntities.GetContext().Organizations.OrderBy(p => p.Title).ToList();
+            DataGridGood.ItemsSource = goods;
+            _itemcount = goods.Count;
+            TextBlockCount.Text = $" Результат запроса: {goods.Count} записей из {goods.Count}";
+        }
+
+        void LoadComboBoxItems()
+        {
+            var categories = OrganizationDBEntities.GetContext().Categories.OrderBy(p => p.Title).ToList();
+            categories.Insert(0, new Category
+            {
+                Title = "Все виды"
+            }
+            );
+            ComboCategory.ItemsSource = categories;
+            ComboCategory.SelectedIndex = 0;
+
+
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // открытие редактирования товара
+            // передача выбранного товара в AddGoodPage
+            Manager.MainFrame.Navigate(new AddEduPage((sender as Button).DataContext as Organization));
+        }
+
+        private void PageIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            //событие отображения данного Page
+            // обновляем данные каждый раз когда активируется этот Page
+            if (Visibility == Visibility.Visible)
+            {
+                DataGridGood.ItemsSource = null;
+                //загрузка обновленных данных
+                OrganizationDBEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                LoadDataGrid();
+            }
+        }
+        /// <summary>
+        /// Метод для фильтрации и сортировки данных
+        /// </summary>
+        private void UpdateData()
+        {
+            // получаем текущие данные из бд
+            //var currentGoods = DataBDEntities.GetContext().Abonements.OrderBy(p => p.CategoryTrainer.Trainer.LastName).ToList();
+
+            var currentData = OrganizationDBEntities.GetContext().Organizations.OrderBy(p => p.Title).ToList();
+            // выбор только тех товаров, которые принадлежат данному производителю
+
+            if (ComboCategory.SelectedIndex > 0)
+                currentData = currentData.Where(p => p.CategoryId == (ComboCategory.SelectedItem as Category).Id).ToList();
+
+            // выбор тех товаров, в названии которых есть поисковая строка
+            currentData = currentData.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if (ComboSort.SelectedIndex >= 0)
+            {
+                // сортировка по возрастанию цены
+                if (ComboSort.SelectedIndex == 0)
+                    currentData = currentData.OrderBy(p => p.Title).ToList();
+                if (ComboSort.SelectedIndex == 1)
+                    currentData = currentData.OrderByDescending(p => p.Title).ToList();
+                if (ComboSort.SelectedIndex == 2)
+                    currentData = currentData.OrderBy(p => p.Rate).ToList();
+                if (ComboSort.SelectedIndex == 3)
+                    currentData = currentData.OrderByDescending(p => p.Rate).ToList();
+                // сортировка по убыванию цены
+            }
+            // В качестве источника данных присваиваем список данных
+
+            // В качестве источника данных присваиваем список данных
+            DataGridGood.ItemsSource = currentData;
+            // отображение количества записей
+            TextBlockCount.Text = $" Результат запроса: {currentData.Count} записей из {_itemcount}";
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void ComboCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+
+        private void ComboSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void BtnAddClick(object sender, RoutedEventArgs e)
+        {
+            // открытие  AddGoodPage для добавления новой записи
+            Manager.MainFrame.Navigate(new AddEduPage(null));
+        }
+
+        private void BtnDeleteClick(object sender, RoutedEventArgs e)
+        {
+            // удаление выбранного товара из таблицы
+            //получаем все выделенные товары
+            var selectedGoods = DataGridGood.SelectedItems.Cast<Organization>().ToList();
+            // вывод сообщения с вопросом Удалить запись?
+            MessageBoxResult messageBoxResult = MessageBox.Show($"Удалить {selectedGoods.Count()} записей???",
+                "Удаление", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            //если пользователь нажал ОК пытаемся удалить запись
+            if (messageBoxResult == MessageBoxResult.OK)
+            {
+                try
+                {
+                    // берем из списка удаляемых товаров один элемент
+                    Organization x = selectedGoods[0];
+                    // проверка, есть ли у товара в таблице о продажах связанные записи
+                    // если да, то выбрасывается исключение и удаление прерывается
+
+                    OrganizationDBEntities.GetContext().Organizations.Remove(x);
+                    //сохраняем изменения
+                    OrganizationDBEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Записи удалены");
+                    List<Organization> clinics = OrganizationDBEntities.GetContext().Organizations.OrderBy(p => p.Title).ToList();
+                    DataGridGood.ItemsSource = null;
+                    DataGridGood.ItemsSource = clinics;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка удаления", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        // отображение номеров строк в DataGrid
+        private void DataGridGoodLoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+    }
+}
---
+++ PharmacyApp/Pages/WorkTimePage.xaml.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EduInstitutesApp.Models;
+using EduInstitutesApp.Windows;
+using System.Data.Entity;
+
+namespace EduInstitutesApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkTimePage.xaml
+    /// </summary>
+    public partial class WorkTimePage : Page
+    {
+        int _itemcount = 0;
+        public WorkTimePage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        void LoadData()
+        {
+            try
+            {
+                DataGridGood.ItemsSource = null;
+                //загрузка обновленных данных
+                OrganizationDBEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                List<WorkTime> goods = OrganizationDBEntities.GetContext().WorkTimes.OrderBy(p => p.Title).ToList();
+                DataGridGood.ItemsSource = goods;
+                _itemcount = goods.Count;
+                TextBlockCount.Text = $" Результат запроса: {goods.Count} записей из {goods.Count}";
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+
+        private void PageIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            //событие отображения данного Page
+            // обновляем данные каждый раз когда активируется этот Page
+            if (Visibility == Visibility.Visible)
+            {
+                LoadData();
+            }
+        }
+
+        private void BtnAddClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+
+                WorkTimeWindow window = new WorkTimeWindow(new WorkTime());
+                if (window.ShowDialog() == true)
+                {
+                    OrganizationDBEntities.GetContext().WorkTimes.Add(window.currentItem);
+                    OrganizationDBEntities.GetContext().SaveChanges();
+                    LoadData();
+                    MessageBox.Show("Запись добавлена", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void BtnDeleteClick(object sender, RoutedEventArgs e)
+        {
+            // удаление выбранного товара из таблицы
+            //получаем все выделенные товары
+            WorkTime selected = (sender as Button).DataContext as WorkTime;
+            // вывод сообщения с вопросом Удалить запись?
+            MessageBoxResult messageBoxResult = MessageBox.Show($"Удалить запись???",
+                "Удаление", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            //если пользователь нажал ОК пытаемся удалить запись
+            if (messageBoxResult == MessageBoxResult.OK)
+            {
+                try
+                {
+
+                    // проверка, есть ли у товара в таблице о продажах связанные записи
+                    // если да, то выбрасывается исключение и удаление прерывается
+                    if (selected.Organizations.Count > 0)
+                        throw new Exception("Есть зависимые записи");
+
+                    OrganizationDBEntities.GetContext().WorkTimes.Remove(selected);
+                    //сохраняем изменения
+                    OrganizationDBEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Записи удалены");
+                    LoadData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка удаления", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        // отображение номеров строк в DataGrid
+        private void DataGridGoodLoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+
+
+        private void TBoxSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateData();
+        }
+        // Поиск товаров конкретного производителя
+        private void ComboTypeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+        /// <summary>
+        /// Метод для фильтрации и сортировки данных
+        /// </summary>
+        private void UpdateData()
+        {
+            // получаем текущие данные из бд
+            var currentGoods = OrganizationDBEntities.GetContext().WorkTimes.OrderBy(p => p.Title).ToList();
+            // выбор только тех товаров, по определенному диапазону скидки
+
+            currentGoods = currentGoods.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            // сортировка
+            if (ComboSort.SelectedIndex >= 0)
+            {
+                // сортировка по возрастанию цены
+                if (ComboSort.SelectedIndex == 0)
+                    currentGoods = currentGoods.OrderBy(p => p.Title).ToList();
+                // сортировка по убыванию цены
+                if (ComboSort.SelectedIndex == 1)
+                    currentGoods = currentGoods.OrderByDescending(p => p.Title).ToList();
+            }
+            // В качестве источника данных присваиваем список данных
+            DataGridGood.ItemsSource = currentGoods;
+            // отображение количества записей
+            TextBlockCount.Text = $" Результат запроса: {currentGoods.Count} записей из {_itemcount}";
+        }
+        // сортировка товаров 
+        private void ComboSortSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateData();
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //  если ни одного объекта не выделено, выходим
+                if (DataGridGood.SelectedItem == null) return;
+                // получаем выделенный объект
+                WorkTime selected = DataGridGood.SelectedItem as WorkTime;
+
+                WorkTimeWindow window = new WorkTimeWindow(selected);
+
+
+                if (window.ShowDialog() == true)
+                {
+                    if (window.currentItem != null)
+                    {
+                        OrganizationDBEntities.GetContext().Entry(window.currentItem).State = EntityState.Modified;
+                        OrganizationDBEntities.GetContext().SaveChanges();
+                        LoadData();
+                        MessageBox.Show("Запись изменена", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                }
+                else
+                {
+                    OrganizationDBEntities.GetContext().Entry(window.currentItem).Reload();
+                    LoadData();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+    }
+}
